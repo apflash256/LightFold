2dcdac045c734e5e815a2a58616bfd2f238f8d01
apflash256 <hypercube256@naver.com>
2024-02-11 12:56:54 +0900

    Write EXR

--- src/lightfold/main/lightfold.cpp
+++ src/lightfold/main/lightfold.cpp
@@ -11,6 +11,6 @@ constexpr int height = 720;
 int main(void) {
 	char filename[] = "image.exr";
 	std::unique_ptr<float[]> img(new float[width * height * 3]);
-	WriteEXR(img, width, height, filename);
+	WriteEXR(std::move(img), width, height, filename);
 	return 0;
 }
